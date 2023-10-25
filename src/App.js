1a6820dad675b1d12288ac52b11e74005d880b3e
Hamza Baig <mail.hamzabaig@gmail.com>
2023-10-25 20:29:17 +0500

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,20 +1,3 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Calculator from './components/Calculator';
-import QuoteDisplay from './components/QuoteDisplay';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<Calculator />} />
-        <Route path="/quote" element={<QuoteDisplay />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
+function App() {}
 
 export default App;
