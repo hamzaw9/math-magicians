58277f50901b8850916bb4e0530d34f91ab06020
Hamza Baig <mail.hamzabaig@gmail.com>
2023-10-25 20:32:28 +0500

    Set up React Router for navigation

--- src/App.js
+++ src/App.js
@@ -1,3 +1,20 @@
-function App() {}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Calculator from './components/Calculator';
+import QuoteDisplay from './components/QuoteDisplay';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calculator" element={<Calculator />} />
+        <Route path="/quote" element={<QuoteDisplay />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 export default App;
